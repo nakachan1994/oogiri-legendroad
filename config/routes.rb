cb999e9e0db3b427598a94a8926cb7ddd64e18cd
nakachan1994 <nakachaaaan@gmail.com>
2021-06-03 06:49:40 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
   get 'homes/guide'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :themes, only: [:new, :create, :index, :show, :destroy]
+  resources :answers, only: [:index, :create, :destroy]
+  resources :likes, only: [:index, :create]
+  resources :contacts, omly: [:new, :create] do
+    collection do
+      post :confirm
+      get :complete
+    end
+  end
 end
