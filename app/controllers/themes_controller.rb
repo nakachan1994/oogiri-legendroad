a1a0b4ec8b14e8984c0ac6574a10c8daf9e38b4f
nakachan1994 <nakachaaaan@gmail.com>
2021-06-03 08:28:45 +0000

    [add]themes/new

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,8 +1,19 @@
 class ThemesController < ApplicationController
   def new
+    @theme = Theme.new
+    @themes = current_user.themes
   end
 
   def create
+    @theme = Theme.new(theme_params)
+    @theme.user_id = current_user.id
+    if @theme.save
+      flash[:notice] = 'お題を提案しました'
+      redirect_back(fallback_location: new_theme_path)
+    else
+      @themes = current_user.themes
+      render :new
+    end
   end
 
   def index
@@ -12,5 +23,11 @@ class ThemesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:image, :content)
   end
 end
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,4 +3,6 @@ class Theme < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :content, length: {minimum: 1, maximum: 100}
+
+  attachment :image
 end
