3128c9678e49ba54dcd527f2a3dba83a2715ff41
nakachan1994 <nakachaaaan@gmail.com>
2021-06-09 13:53:57 +0000

    [update]user,rank

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,18 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    # @users = User.joins(:answers).where(answers: {created_at: Time.now.all_month}).group(:id).order('count(answers.user_id) desc')
-    # @users = User.joins(:answers).where(answers: {created_at: Time.now.all_month}).group(:id).sort {|a,b| b.answers.count <=> a.answers.count}
-    # @users = User.all.sort{|a,b| b.likes.count <=> a.likes.count}
-    # @users = User.joins(:likes).where(answer_id: user.ids).group(:id).sort {|a,b| b.answers.likes.count <=> a.answers.likes.count}
-    # @users = Answer.left_joins(:likes).group(:user_id).order(Arel.sql('COUNT(likes.id)'))
-    #@answers = Answer.left_joins(:likes).group(:user_id).order(Arel.sql('COUNT(likes.id) DESC'))
     # 経験値順に並び替え
-    likes_count = {}
+    total_exp_count = {}
     User.all.each do |user|
-      likes_count.store(user , User.total_exp(user))
+      total_exp_count.store(user , User.total_exp(user))
     end
-    @users = likes_count.sort_by{ |_,v| v}.reverse.to_h.keys
+    @total_exp_rank = total_exp_count.sort_by{ |_,v| v}.reverse.to_h.keys
+    # 獲得いいね順に並び替え
+    answer_like_count = {}
+    User.all.each do |user|
+      answer_like_count.store(user , User.answer_like_count(user))
+    end
+    @answer_likes_rank = answer_like_count.sort_by{ |_,v| v}.reverse.to_h.keys
   end
 
   def show
