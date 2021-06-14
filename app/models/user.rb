107929e405b216925bc92b9f9b5a92482e76d7c1
nakachan1994 <nakachaaaan@gmail.com>
2021-06-14 08:37:18 +0000

    [update]user/showレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true, length: {maximum: 15}
+  validates :name, presence: true, uniqueness: true, length: {maximum: 12}
   validates :email, presence: true, uniqueness: true
 
   attachment :profile_image
