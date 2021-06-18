01ae316bf6190ae989b9ddbee06bc445afcc5ebf
nakachan1994 <nakachaaaan@gmail.com>
2021-06-18 08:07:40 +0000

    [update]回答のいいね順wo同率順位表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,14 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    # 経験値順に並び替え
+    # 管理者以外経験値順に並び替え
     total_exp_count = {}
     User.where(admin: false).each do |user|
       total_exp_count.store(user , User.total_exp(user))
     end
     @total_exp_rank = total_exp_count.sort_by{ |_,v| v}.reverse.to_h.keys
     @total_exp_rank = Kaminari.paginate_array(@total_exp_rank).limit(20)
-    # 獲得いいね順に並び替え
+    # 管理者以外獲得いいね順に並び替え
     answer_like_count = {}
     User.where(admin: false).each do |user|
       answer_like_count.store(user , User.answer_like_count(user))
