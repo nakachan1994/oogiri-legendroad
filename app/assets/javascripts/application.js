294241bc2bce51a902748955af7921f6f98664b1
nakachan1994 <nakachaaaan@gmail.com>
2021-06-21 13:52:30 +0000

    [add]topビューロード画面

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,3 +86,11 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
+// ローディング画面の表示
+document.addEventListener('turbolinks:load', function() {
+  $(window).on('load',function(){
+    $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#loading_box").delay(1200).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
+  });
+});
