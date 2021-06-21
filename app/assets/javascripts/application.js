f3fe3f6dcc5c56dee6cdbbb105b3a3c0d8267a7f
nakachan1994 <nakachaaaan@gmail.com>
2021-06-21 07:48:05 +0000

    [add]guideのQ＆A/jqueryアコーディオンメニュー化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,3 +74,13 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
+// アコーディオンメニュー
+$(function(){
+  $('.js-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 200);
+  });
+});
